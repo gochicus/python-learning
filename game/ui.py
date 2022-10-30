f1729517c14af2e1a8fcf6b8a05c4e6e00e0a4de
gochicus <gochicus@gmail.com>
2022-10-30 14:18:04 +0900

    logic in progress

--- game/ui.py
+++ game/ui.py
@@ -1,11 +1,16 @@
+from cgitb import text
 from tkinter import *
+import random
 
 root = Tk()
 label = Label (width=40, text='Крестики-нолики')
+buttons = [Button(width=20,height=5,bg='gray',text='',command=lambda x=i:get_move(x)) for i in range(9)]
 
-
+def get_move(x):
+        buttons[x].config(text='X',bg='red')
+        print(buttons[x].text)
+  
 def defaine_interface():
-    buttons = [Button(width=20,height=5,bg='gray') for i in range(9)]
     label.grid(row=0,column=0,columnspan=3)
     row = 1
     column = 0
@@ -16,4 +21,5 @@ def defaine_interface():
             row+=1
             column =0
     root.mainloop()
+    
 defaine_interface()
